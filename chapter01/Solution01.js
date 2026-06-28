f668db91f2c7b05feca32fc36060922053bc9378
apenin <alexzander.88@mail.ru>
2026-06-28 16:05:45 +0300

    Solution01 of Chapter01

---
+++ chapter01/Solution01.js
@@ -0,0 +1,13 @@
+console.log(NaN + 0); // NaN
+console.log(Infinity + 0); // Infinity
+console.log(false + 0); // 0
+console.log(true + 0); // 1
+console.log(null + 0); // 0
+console.log(undefined + 0); // NaN
+
+console.log(NaN + ''); // 'NaN'
+console.log(Infinity + ''); // 'Infinity'
+console.log(false + ''); // 'false'
+console.log(true + ''); // 'true'
+console.log(null + ''); // 'null'
+console.log(undefined + ''); // 'undefined'
