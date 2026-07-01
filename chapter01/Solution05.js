802b6923511701015bdaf011c5308175e3b5d492
apenin <alexzander.88@mail.ru>
2026-07-01 15:28:03 +0300

    Solution05 of Chapter01

---
+++ chapter01/Solution05.js
@@ -0,0 +1,11 @@
+const str1 = '\\\\';
+const str2 = "\\\\";
+const str3 = `\\\\`;
+const str4 = "\u{005C}\u{005C}";
+const str5 = '\\' + '\\';
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(str5);
