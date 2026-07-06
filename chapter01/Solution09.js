903f5ba14d00bf3e7d4b6ab7b2d0ca27df332e9a
apenin <alexzander.88@mail.ru>
2026-07-06 18:31:46 +0300

    Solution09 of Chapter01

---
+++ chapter01/Solution09.js
@@ -0,0 +1,12 @@
+const arr = [];
+arr[0] = 'a';
+arr[0.5] = 'b';
+arr[1] = 'c';
+arr[1.5] = 'd';
+arr[2] = 'e';
+
+console.log(arr[0]);
+console.log(arr[0.5]);
+console.log(arr[1]);
+console.log(arr[1.5]);
+console.log(arr[2]);
